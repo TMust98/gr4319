7db7767a8de3757af859cec71eecca631a516735
unknown <timmust98@gmail.com>
2024-03-20 00:53:43 +0300

    flask-3

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index', methods=['GET','POST'])
 def index():
     return render_template('index.html', title='тесты')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Вход пользователя {form.username.data} с паролем {form.password.data}. Запомнить: {form.remember_me.data}', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('login.html', title = 'Вход', form = form)

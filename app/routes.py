9cefb2043713af78848b61dff067b1c52a849913
unknown <timmust98@gmail.com>
2024-03-22 21:45:07 +0300

    flask-4

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, send_from_directory, request
 from app.forms import LoginForm
+import os
+from app.models import User
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'logo.png')
 
 @app.route('/')
 @app.route('/index', methods=['GET','POST'])
@@ -10,9 +18,33 @@ def index():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Вход пользователя {form.username.data} с паролем {form.password.data}. Запомнить: {form.remember_me.data}', 'success')
-        return redirect(url_for('login'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Неправильный логин или пароль', 'danger')
+            flash(f'{form.username.data} ///// {generate_password_hash(form.password.data)}')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page:
+            next_page = url_for('index')
+        flash('Успешный вход', 'success')
+        return redirect(next_page)
 
     return render_template('login.html', title = 'Вход', form = form)
+
+
+@app.route('/lk', methods=['GET','POST'])
+@login_required
+def lk():
+    return render_template('lk.html', title='Личный кабинет')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('Вы вышли из системы', 'warning')
+    return redirect('index')
